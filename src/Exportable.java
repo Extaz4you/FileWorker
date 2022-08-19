1e937b22061af9fe0dc08da1592493aa1797b331
Extaz4you <nikitqalevioffanov@gmaol.com>
2022-08-19 12:05:52 +0300

    Initial commit

---
+++ src/Exportable.java
@@ -0,0 +1,6 @@
+
+import java.util.ArrayList;
+
+public interface Exportable {
+    public void exportAs(ArrayList<Task> tasks, String path);
+}
---
+++ src/FormatXML.java
@@ -0,0 +1,16 @@
+import com.FileWorker.Task;
+import com.FileWorker.TaskList;
+
+import java.util.ArrayList;
+
+public class FomatXML extends Importable.TextFormat {
+    @Override
+    public void exportAs(ArrayList<Task> tasks, String path) {
+
+    }
+
+    @Override
+    public ArrayList<Task> importFrom(String path) {
+        return null;
+    }
+}
